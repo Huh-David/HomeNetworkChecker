caaef5da1694c444325b27320d28f161e6138e1d
David <D.Huh@t-online.de>
2021-05-21 22:37:10 +0200

    :recycle: Refactored main method and renamed person_checker.py to home_network_checker.py
    - moved parts of main() in separate methods

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from person_checker import HomeNetworkChecker
+from home_network_checker import HomeNetworkChecker
 import json
 import os
 
@@ -56,24 +56,11 @@ def read_json(filename):
     return json_content
 
 
-def main():
-    # On first start or if files are missing the write_json() methods will be executed
-    if not os.path.isfile('persons.json') or not os.path.isfile('fritz_logins.json'):
-        print('Some configuration files are missing.\nShould they be created automatically?')
-        while True:
-            create_automatically = input('Y/N\n')
-            if create_automatically == "y" or create_automatically == "Y":
-                print('The required files are being generated right now...')
-                write_json()
-                print('Files have been created')
-                break
-            elif create_automatically == "n" or create_automatically == "N":
-                print(
-                    'Then go ahead and change the settings in write_json()! \nOr create the required files yourself... :(')
-                exit()
-            else:
-                print('No valid input\n')
-
+def get_console_input():
+    """
+    Just dialog to setup how long the script should be run
+    :return: seconds_until_terminate, tracking_period
+    """
     seconds_until_terminate = None
     while not type(seconds_until_terminate) == int:
         seconds_until_terminate = input('How long should the script be run in seconds?\n')
@@ -90,18 +77,50 @@ def main():
         except:
             pass
 
+    return seconds_until_terminate, tracking_period
+
+
+def check_config_files():
+    """
+    # On first start or if files are missing the write_json() methods will / can be executed
+    """
+    if not os.path.isfile('persons.json') or not os.path.isfile('fritz_logins.json'):
+        print('Some configuration files are missing.\nShould they be created automatically?')
+        while True:
+            create_automatically = input('Y/N\n')
+            if create_automatically == "y" or create_automatically == "Y":
+                print('The required files are being generated right now...')
+                write_json()
+                print('Files have been created')
+                break
+            elif create_automatically == "n" or create_automatically == "N":
+                print(
+                    'Then go ahead and change the settings in the write_json() method!\n'
+                    'Or create the required files yourself... :(')
+                exit()
+            else:
+                print('No valid input\n')
+
+
+def main():
+    """
+    This method will be executed by default
+    :return: List of all given persons with indicator if person is at home or not
+    """
+    check_config_files()  # checks if files are missing
+    seconds_until_terminate, tracking_period = get_console_input()  # user input for length of script time
+
     # persons from whom you know the ip and want to track
     # rather choose the ips of mobile phones because these should be a good indicator if somebody is at home
     persons_json = read_json('persons.json')
-
-    # in case you have multiple fritzrepeaters you need to add them all
-    fritz_logins_json = read_json('fritz_logins.json')
-
     persons: [[]] = []
-    fritz_logins: [[]] = []
 
     for x in persons_json['known_ips']:
         persons.append([x['ip'], x['name']])
+
+    # in case you have multiple fritzrepeaters you need to add them all
+    fritz_logins_json = read_json('fritz_logins.json')
+    fritz_logins: [[]] = []
 
     for x in fritz_logins_json['fritz_logins']:
         fritz_logins.append([x['ip'], x['password']])
